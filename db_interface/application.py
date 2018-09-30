d12a6b8256d8afffe21b32d77389ea85c601a28a
SuperPrower <dya.eshshmai@gmail.com>
2018-09-30 14:22:02 +0300

    Temporarily disable migrations

--- db_interface/application.py
+++ db_interface/application.py
@@ -2,11 +2,11 @@ import db_interface.config
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object("db_interface.config.DevelopConfig")
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
