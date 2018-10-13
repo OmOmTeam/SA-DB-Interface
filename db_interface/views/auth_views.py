03147a1183c1c0d35602a6a1bfa1fcd73cce316e
SuperPrower <dya.eshshmai@gmail.com>
2018-10-13 18:48:31 +0300

    add some auth routes

--- db_interface/views/auth_views.py
+++ db_interface/views/auth_views.py
@@ -1,17 +1,48 @@
 from flask import jsonify, request
 from db_interface.application import app, mysql
 
+"""
+    response = {'error': 'none'}
+    if request.is_json:
+        pass
 
-@app.route('/get_access_rights')
+    else:
+        response['error'] = 'JSON expected'
+
+    return jsonify(response)
+"""
+
+
+@app.route('/register_new_employee', methods=['POST', ])
+def register_new_employee():
+    response = {'error': 'none'}
+
+    if not request.is_json:
+        response['error'] = 'JSON expected'
+        return jsonify(response)
+
+    try:
+        j = request.get_json()
+
+        cur = mysql.connection.cursor()
+
+        args = (j['login'], j['password'], j['role'])
+        cur.callproc('LogisticCompany.Registration', args)
+
+    except KeyError:
+        response['error'] = 'Invalid JSON'
+
+    return jsonify(response)
+
+
+@app.route('/get_access_rights', methods=['GET', ])
 def get_access_rights():
     cur = mysql.connection.cursor()
 
-    args = ['Admin', 'admin']
-    cur.callproc('GetAccessRights', args)
+    args = ('Admin', 'admin', 0)
+    cur.callproc('LogisticCompany.GetAccessRights', args)
+    cur.execute('SELECT @_LogisticCompany.GetAccessRights_2')
 
-    res = ''
+    res = cur.fetchone()
 
-    for result in cur.stored_results():
-        res += result.fetchall()
-
-    return str(res)
+    return str(res[0])
--- flask.sh
+++ flask.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export FLASK_APP=db_interface
+export FLASK_ENV=development
 flask "$@"
