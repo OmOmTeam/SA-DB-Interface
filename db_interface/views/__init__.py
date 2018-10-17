75f9c907864334d328eb03f24d4fc3d14c4298dd
SuperPrower <dya.eshshmai@gmail.com>
2018-10-17 21:55:15 +0300

    add login

--- db_interface/views/__init__.py
+++ db_interface/views/__init__.py
@@ -1,5 +1,5 @@
 from db_interface.views.mobile_app_views import *
 from db_interface.views.test_views import *
-from db_interface.views.auth_views import *
 from db_interface.views.order_views import *
+from db_interface.views.employee_views import *
 # and so on
--- db_interface/views/employee_views.py
+++ db_interface/views/employee_views.py
@@ -13,6 +13,39 @@ from db_interface.application import app, mysql
 """
 
 
+@app.route('/login', methods=['POST', ])
+def login():
+    response = {'error': 'none'}
+
+    if not request.is_json:
+        response['error'] = 'JSON expected'
+        return jsonify(response)
+
+    try:
+        j = request.get_json()
+        login, passhash = j['login'], j['password_hash']
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            SELECT id, AccessRights
+            FROM LogisticCompany.Employees
+            WHERE Login = %s AND PassHash = %s;
+            """, (login, passhash,))
+
+        employee_id, access_rights = cur.fetchone()
+
+        if employee_id:
+            response['access_rights'] = access_rights
+
+        else:
+            response['error'] = 'Invalid Login or Password'
+
+    except KeyError:
+        response['error'] = 'Invalid JSON'
+
+    return jsonify(response)
+
+
 @app.route('/register_new_employee', methods=['POST', ])
 def register_new_employee():
     response = {'error': 'none'}
@@ -26,7 +59,7 @@ def register_new_employee():
 
         cur = mysql.connection.cursor()
 
-        args = (j['login'], j['password'], j['role'])
+        args = (j['login'], j['password_hash'], j['role'])
         cur.callproc('LogisticCompany.Registration', args)
 
     except KeyError:
