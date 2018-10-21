c10dadf6f9b8942e6c1b28a914efd4074f1350d5
SuperPrower <dya.eshshmai@gmail.com>
2018-10-21 09:58:39 +0300

    push before rework

--- db_interface/views/mobile_app_views.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify, request
-from db_interface.application import app
-
-
-@app.route('/')
-def index():
-    return 'Hello, world!'
--- db_interface/views/order_views.py
+++ db_interface/views/order_views.py
@@ -13,6 +13,45 @@ from db_interface.application import app, mysql
 """
 
 
+@app.route('/orders', methods=['GET', ])
+def get_orders():
+    response = {'error': 'none', 'orders': []}
+    cur = mysql.connection.cursor()
+
+    cur.execute('CALL LogisticCompany.GetOrders')
+
+    res = cur.fetchall()
+    response['orders'] = unfold_orders(res)
+
+    return jsonify(response)
+
+
+@app.route('/get_order_by_id', methods=['GET', ])
+def get_order_by_id():
+    response = {'error': 'none', 'orders': []}
+
+    if not request.is_json:
+        response['error'] = 'JSON expected'
+        return jsonify(response)
+
+    else:
+        cur = mysql.connection.cursor()
+
+        try:
+            order_id = request.get_json['order_id']
+
+        except KeyError:
+            response['error'] = 'Invalid JSON'
+            return jsonify(response)
+
+        cur.execute('CALL LogisticCompany.GetOrderById', (order_id, ))
+
+        res = cur.fetchall()
+        response['orders'] = unfold_orders(res)
+
+    return jsonify(response)
+
+
 @app.route('/orders_to_approve', methods=['GET', ])
 def orders_to_approve():
 
@@ -22,8 +61,20 @@ def orders_to_approve():
     cur.execute('CALL LogisticCompany.GetOrdersOnValidating')
 
     res = cur.fetchall()
+    response['orders'] = unfold_orders(res)
 
-    for order in res:
+    return jsonify(response)
+
+
+def unfold_orders(query_results):
+    """
+    Since at least 3 queries return orders in following format,
+    I thinks it's a good idea to have one function
+    for unfolding similar query results into dictionary
+    """
+    orders = []
+
+    for order in query_results:
         order_id, sender_name, sender_surname, sender_telephone, \
                 sender_country, sender_region, sender_city, sender_street, \
                 sender_building_num, sender_addition_info, \
@@ -32,7 +83,7 @@ def orders_to_approve():
                 receiver_street, receiver_building_num, \
                 receiver_addition_info, \
                 attached_notes, status, delivery_type, pickup_type = order
-        response['orders'].append({
+        orders.append({
                     'id': order_id,
                     'order_status': status,
                     'sender': {
@@ -66,4 +117,4 @@ def orders_to_approve():
                     'attached_notes': attached_notes
                 })
 
-    return jsonify(response)
+        return orders
